390866852435194d5b6844510618548a0fa3ea19
Vladimir Panteleev <git@thecybershadow.net>
2020-06-27 21:54:21 +0000

    [dlang] Add favicon

--- extensions/Dlang/Extension.pm
+++ extensions/Dlang/Extension.pm
@@ -13,4 +13,18 @@ use warnings;
 
 use base qw(Bugzilla::Extension);
 
+sub app_startup {
+  my ($self, $args) = @_;
+  my $app = $args->{app};
+  my $r   = $app->routes;
+
+  delete $app->static->extra->{'favicon.ico'};
+  $r->get(
+    '/favicon.ico' => sub {
+      my $c = shift;
+      $c->reply->file($c->app->home->child('extensions/Dlang/web/images/favicon.ico'));
+    }
+  );
+}
+
 __PACKAGE__->NAME;
